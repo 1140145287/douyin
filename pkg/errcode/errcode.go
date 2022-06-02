57f1d805f319244aec602511faa649f5b10b533f
Yan qiu <dbfires@gmail.com>
2022-06-02 12:29:25 +0800

    添加统一错误码，以及修复上个版本一些纰漏

---
+++ pkg/errcode/errcode.go
@@ -0,0 +1,80 @@
+package errcode
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Error struct {
+	code    int      `json:"code"`
+	msg     string   `json:"msg"`
+	details []string `json:"details"`
+}
+
+//全局错误码
+var codes = map[int]string{}
+
+func NewError(code int, msg string) *Error {
+	if _, ok := codes[code]; ok {
+		panic(fmt.Sprintf("错误码 %d 已经存在，请更换一个", code))
+	}
+	codes[code] = msg
+	return &Error{code: code, msg: msg}
+}
+
+//输出相关的错误信息
+func (e *Error) Error() string {
+	return fmt.Sprintf("错误码：%d, 错误信息：%s", e.Code(), e.Msg())
+}
+
+//输出对应的error值
+func (e *Error) Code() int {
+	return e.code
+}
+
+//输出err对应的中文信息
+func (e *Error) Msg() string {
+	return e.msg
+}
+
+func (e *Error) Msgf(args []interface{}) string {
+	return fmt.Sprintf(e.msg, args...)
+}
+
+//输出对应的detail内的细节
+func (e *Error) Details() []string {
+	return e.details
+}
+
+//输出更多详细细节
+func (e *Error) WithDetails(details ...string) *Error {
+	newError := *e
+	newError.details = []string{}
+	for _, d := range details {
+		newError.details = append(newError.details, d)
+	}
+
+	return &newError
+}
+
+func (e *Error) StatusCode() int {
+	switch e.Code() {
+	case Success.Code():
+		return http.StatusOK
+	case ServerError.Code():
+		return http.StatusInternalServerError
+	case InvalidParams.Code():
+		return http.StatusBadRequest
+	case UnauthorizedAuthNotExist.Code():
+		fallthrough
+	case UnauthorizedTokenError.Code():
+		fallthrough
+	case UnauthorizedTokenGenerate.Code():
+		fallthrough
+	case UnauthorizedTokenTimeout.Code():
+		return http.StatusUnauthorized
+	case TooManyRequests.Code():
+		return http.StatusTooManyRequests
+	}
+	return http.StatusInternalServerError
+}
