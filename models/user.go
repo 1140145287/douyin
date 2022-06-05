0af144260c3bea271c4966dedf7ea97d6db7505d
Yan qiu <dbfires@gmail.com>
2022-06-05 22:15:54 +0800

    修改了userInfo下的部分逻辑

--- models/user.go
+++ models/user.go
@@ -3,11 +3,11 @@ package models
 type User struct {
 	Id            int64  `json:"id,omitempty" gorm:"column:user_id"`
 	Name          string `json:"name,omitempty" gorm:"column:name"`
-	PassWord      string `gorm:"column:password" json:"password,omitempty"`
+	PassWord      string `gorm:"column:password" json:"-"`
 	FollowCount   int64  `json:"follow_count,omitempty" gorm:"column:follow_count"`
 	FollowerCount int64  `json:"follower_count,omitempty" gorm:"column:follower_count"`
 	IsFollow      bool   `json:"is_follow,omitempty" gorm:"column:is_follow"`
-	Salt          string `json:"salt,omitempty" gorm:"column:salt"`
+	Salt          string `json:"-" gorm:"column:salt"`
 	Token         string `gorm:"-" json:"token,omitempty"`
 }
 
